f382c96eacef6e7dd0c00931179f6d5ebf78ac4d
HK-ankithkdigiverse <ankithkdigiverse@gmail.com>
2026-03-18 17:37:39 +0530

    user/getMe

--- src/Routes/user.ts
+++ src/Routes/user.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { adminJwt } from "../helper";
+import { adminJwt, userJwt } from "../helper";
 import { userController } from "../controllers";
 
 const router = Router();
 
+router.get("/getme", userJwt, userController.getMe);
 router.put("/edit", adminJwt, userController.updateUser);
 router.delete("/:id", adminJwt, userController.deleteUser);
 router.get("/", adminJwt, userController.getUsers);
